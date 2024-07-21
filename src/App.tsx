05ba57f5d10bc4707dd66e50a4adb36b7570523b
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2024-07-21 14:12:35 +0300

    TS-3/ setup tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { FC } from "react";
 import "./App.css";
+import queryClient from "./cache/queryClient";
 import AppRoutes from "./Routes/AppRoutes";
 
 const App: FC = () => {
-  return <AppRoutes />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRoutes />
+      <ReactQueryDevtools
+        initialIsOpen={false}
+        position="right"
+        buttonPosition="bottom-right"
+      />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
