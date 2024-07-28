d776d71c5a954a056a69240394e2bdb8b42024e1
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2024-07-29 02:07:50 +0300

    TS-7/ handle hotel card click in search results page

--- src/pages/SearchResults/components/ResultsList.tsx
+++ src/pages/SearchResults/components/ResultsList.tsx
@@ -1,15 +1,18 @@
-import { Skeleton, Stack, useMediaQuery } from "@mui/material";
+import { Box, Skeleton, Stack, useMediaQuery } from "@mui/material";
 import { FC } from "react";
 import AmenityChip from "src/components/AmenityChip";
 import HotelCard from "src/components/HotelCard";
 import useSearchResultsContext from "../context/useSearchResultsContext";
 import { mapSearchResultToHotel } from "../utils";
 import EmptySearch from "./EmptySearch";
+import { useNavigate } from "react-router-dom";
 
 const ResultsList: FC = () => {
   const {
     state: { searchResults, loading },
   } = useSearchResultsContext();
+
+  const navigate = useNavigate();
 
   const sm = useMediaQuery("(min-width: 900px)");
 
@@ -29,6 +32,10 @@ const ResultsList: FC = () => {
 
   if (!searchResults || searchResults.length === 0) return <EmptySearch />;
 
+  const handleCardClick = (hotelId: number) => {
+    navigate(`/me/hotels/${hotelId}`);
+  };
+
   return (
     <Stack
       gap={1}
@@ -39,21 +46,26 @@ const ResultsList: FC = () => {
       justifyContent={{ sm: "center", md: "flex-start" }}
     >
       {searchResults.map((hotel) => (
-        <HotelCard
-          key={hotel.hotelId}
-          hotel={mapSearchResultToHotel(hotel)}
-          {...(sm ? { horizontal: true } : {})}
+        <Box
+          onClick={() => handleCardClick(hotel.hotelId)}
+          sx={{ cursor: "pointer" }}
         >
-          <HotelCard.Rating />
-          <HotelCard.Heading />
-          <HotelCard.Description />
-          <Stack flexDirection="row" flexWrap="wrap" pb={1.5} gap={1}>
-            {hotel.amenities.map(({ name }) => (
-              <AmenityChip key={name} label={name} />
-            ))}
-          </Stack>
-          <HotelCard.InfoCard showPrice singlePriceBound />
-        </HotelCard>
+          <HotelCard
+            key={hotel.hotelId}
+            hotel={mapSearchResultToHotel(hotel)}
+            {...(sm ? { horizontal: true } : {})}
+          >
+            <HotelCard.Rating />
+            <HotelCard.Heading />
+            <HotelCard.Description />
+            <Stack flexDirection="row" flexWrap="wrap" pb={1.5} gap={1}>
+              {hotel.amenities.map(({ name }) => (
+                <AmenityChip key={name} label={name} />
+              ))}
+            </Stack>
+            <HotelCard.InfoCard showPrice singlePriceBound />
+          </HotelCard>
+        </Box>
       ))}
     </Stack>
   );
