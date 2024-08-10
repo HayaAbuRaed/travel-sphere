50cd02ad3273aa199eebb8faf7595d2fb6e09426
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2024-08-10 21:45:04 +0300

    TS-12/ add .shape() to schema objects

--- src/pages/CitiesManagement/schema.ts
+++ src/pages/CitiesManagement/schema.ts
@@ -1,7 +1,7 @@
 import * as Yup from "yup";
 import { AddCityRequest } from "./API/types";
 
-export const validationSchema = Yup.object<AddCityRequest>({
+export const validationSchema = Yup.object<AddCityRequest>().shape({
   name: Yup.string().required("Name is required"),
   description: Yup.string().required("Description is required"),
 });
