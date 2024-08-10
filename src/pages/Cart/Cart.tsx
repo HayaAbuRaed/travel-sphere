d9c7e4395607251be1711fee2b75869926c82d34
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2024-08-10 06:06:10 +0300

    TS-12/ Add animations for empty cart and room not found

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,11 +1,29 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
+import Lottie from "lottie-react";
 import { FC } from "react";
 import RoomCard from "src/components/RoomCard";
 import { selectCart } from "src/features/cart/selectors";
 import { useAppSelector } from "src/store/hooks";
+import emptyCart from "src/animations/emptyCart.json";
 
 const Cart: FC = () => {
   const { items } = useAppSelector(selectCart);
+
+  if (items.length === 0) {
+    return (
+      <Stack
+        height="calc(100vh - 64px - 48px)"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Lottie animationData={emptyCart} style={{ minWidth: "300px" }} />
+        <Typography textAlign="center" fontWeight={500}>
+          Your cart is empty. <br />
+          Please add rooms to the cart to checkout
+        </Typography>
+      </Stack>
+    );
+  }
 
   return (
     <Grid container direction="column" alignItems="center" p={5} pt={4} pr={3}>
