e4b65a425773effbe967672e60568ced0deb2722
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2024-08-10 21:35:34 +0300

    TS-12/ wrapped normal user pages with routeHOC

--- src/pages/Cart/index.ts
+++ src/pages/Cart/index.ts
@@ -1,1 +1,7 @@
-export { default } from "./Cart";
+import routeHOC from "src/Routes/HOCs/routeHOC";
+import Cart from "./Cart";
+
+export default routeHOC({
+  title: "Cart",
+  pageAccessName: "Cart",
+})(Cart);
--- src/pages/Checkout/index.ts
+++ src/pages/Checkout/index.ts
@@ -1,1 +1,7 @@
-export { default } from "./Checkout";
+import routeHOC from "src/Routes/HOCs/routeHOC";
+import Checkout from "./Checkout";
+
+export default routeHOC({
+  title: "Checkout",
+  pageAccessName: "Checkout",
+})(Checkout);
