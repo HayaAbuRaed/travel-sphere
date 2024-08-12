9086f4e18477a54c5e0b76fd499a8698f5a338c3
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2024-08-12 04:22:46 +0300

    TS-13/ build splash screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "./App.tsx";
+// import App from "./App.tsx";
 import "./index.css";
 import store from "./store";
 import TravelSphereThemeProvider from "./style/TravelSphereThemeProvider";
+import SplashScreen from "src/components/SplashScreen";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
@@ -15,7 +16,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           path="/*"
           element={
             <Provider store={store}>
-              <App />
+              {/* <App /> */}
+              <SplashScreen />
             </Provider>
           }
         />
