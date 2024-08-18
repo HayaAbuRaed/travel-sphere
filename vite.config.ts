00e7543ac1fa657e75b98c6a472590b29ac6b278
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2024-08-18 05:03:34 +0300

    TS-16/ fix build bug in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import EnvironmentPlugin from "vite-plugin-environment";
 
+delete process.env["CommonProgramFiles(x86)"];
+delete process.env["ProgramFiles(x86)"];
+
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), EnvironmentPlugin("all")],
 });
